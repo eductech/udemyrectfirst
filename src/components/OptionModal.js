49066deba23e102bd884d6524fcea40113beddfc
Aliaksander Zamirouski <studiaformanta@yandex.ru>
2018-07-30 00:29:42 +0300

    lesson 75

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -6,10 +6,12 @@ const OptionModal = (props) => (
     isOpen={!!props.selectedOption}
     contentLabel="Selected Option"
     onRequestClose={props.closeModal}
+    closeTimeoutMS={200}
+    className="modal"
   >
-    <h3>Slected option</h3>
-    {props.selectedOption && <p>{props.selectedOption}</p>}
-    <button onClick={props.closeModal}>Okay</button>
+    <h3 className="modal__title">Slected option</h3>
+    {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+    <button className="button" onClick={props.closeModal}>Okay</button>
   </Modal>
 );
 
